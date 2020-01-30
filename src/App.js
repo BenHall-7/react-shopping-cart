8725766834ba6d31292781cac600086c9d5b8537
BenHall-7 <benjaminjahall@gmail.com>
2020-01-29 22:34:07 -0500

    Add CartContext to components

--- src/App.js
+++ src/App.js
@@ -22,9 +22,11 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<Navigation/>
-				<Route exact path="/" component={Products}/>
-				<Route path="/cart" component={ShoppingCart}/>
+				<CartContext.Provider value={{ cart }}>
+					<Navigation/>
+					<Route exact path="/" component={Products}/>
+					<Route path="/cart" component={ShoppingCart}/>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
